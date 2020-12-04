8c4a75995964e018ab9113f350b4f36c8b10ac1e
Elke Heymans <el.heymans@gmail.com>
2020-12-04 14:23:56 +0100

    Day 04 solution

--- utils/readFile.ts
+++ utils/readFile.ts
@@ -1,5 +1,12 @@
 import { readFileSync } from "fs";
 
-export default function readFile(day: string, part: "a" | "b"): string[] {
-  return readFileSync(`days/${day}/${day}${part}.txt`, { encoding: "utf8" }).toString().split("\n");
+export default function readFile(
+  day: string,
+  part: "a" | "b",
+  split: boolean = true
+): string[] | string {
+  const contents = readFileSync(`days/${day}/${day}${part}.txt`, {
+    encoding: "utf8",
+  }).toString();
+  return split ? contents.split("\n") : contents;
 }
