405e352678f51d326d83629285fe84af62b34297
Elke Heymans <el.heymans@gmail.com>
2020-12-17 09:34:27 +0100

    Day 17 solution

---
+++ days/17/index.ts
@@ -0,0 +1,177 @@
+import readFile from "../../utils/readFile";
+
+interface Cube {
+  x: number;
+  y: number;
+  z: number;
+  w?: number;
+  active: boolean;
+}
+
+const getKey = ({ x, y, z, w }: Cube): string => `${x},${y},${z},${w}`;
+
+const countActiveNeighbours = (
+  cubes: Map<string, Cube>,
+  { x, y, z, w }: { x: number; y: number; z: number; w: number },
+  delta: number
+): number => {
+  let result = 0;
+  for (let testCube of expanded(x, y, z, w, delta)) {
+    const key = getKey(testCube);
+    if (cubes.has(key) && key !== getKey({ x, y, z, w, active: false })) {
+      result += cubes.get(key).active ? 1 : 0;
+    }
+  }
+  return result;
+};
+
+function* expanded(
+  oldX: number,
+  oldY: number,
+  oldZ: number,
+  oldW: number,
+  delta: number
+) {
+  for (let x = oldX - 1; x <= oldX + 1; x++) {
+    for (let y = oldY - 1; y <= oldY + 1; y++) {
+      for (let z = oldZ - 1; z <= oldZ + 1; z++) {
+        for (let w = oldW - delta; w <= oldW + delta; w++) {
+          yield { x, y, z, w, active: false };
+        }
+      }
+    }
+  }
+}
+
+function runPartA() {
+  const data = readFile("17", "a") as string[];
+
+  let cubes: Map<string, Cube> = new Map();
+  data
+    .map((line: string, y: number) =>
+      line.split("").map(
+        (state, x): Cube => ({
+          x,
+          y,
+          z: 0,
+          w: 0,
+          active: state === "#",
+        })
+      )
+    )
+    .reduce((acc, val) => acc.concat(val), [])
+    .forEach((cube) => cubes.set(getKey(cube), cube));
+
+  const cycle = (cubes: Map<string, Cube>, amount: number): number => {
+    const inner = (
+      cubes: Map<string, Cube>,
+      delta: number
+    ): Map<string, Cube> => {
+      if (delta === amount) {
+        return cubes;
+      } else {
+        const newCubes = new Map<string, Cube>();
+        for (const [key, cube] of cubes) {
+          for (let newCube of expanded(cube.x, cube.y, cube.z, 0, 0)) {
+            if (newCubes.has(getKey(newCube))) {
+              continue;
+            }
+            const activeNeighbours = countActiveNeighbours(cubes, newCube, 0);
+            const oldCube = cubes.get(getKey(newCube)) || {
+              ...newCube,
+              active: false,
+            };
+            if (
+              oldCube.active &&
+              (activeNeighbours === 2 || activeNeighbours === 3)
+            ) {
+              newCubes.set(getKey(newCube), {
+                ...newCube,
+                active: true,
+              });
+            } else if (!oldCube.active && activeNeighbours === 3) {
+              newCubes.set(getKey(newCube), {
+                ...newCube,
+                active: true,
+              });
+            }
+          }
+        }
+        return inner(newCubes, delta + 1);
+      }
+    };
+
+    return Array.from(inner(cubes, 0))
+      .map(([_, cube]) => cube)
+      .reduce((prev, cube) => prev + (cube.active ? 1 : 0), 0);
+  };
+  return cycle(cubes, 6);
+}
+
+function runPartB() {
+  const data = readFile("17", "b") as string[];
+
+  let cubes: Map<string, Cube> = new Map();
+  data
+    .map((line: string, y: number) =>
+      line.split("").map(
+        (state, x): Cube => ({
+          x,
+          y,
+          z: 0,
+          w: 0,
+          active: state === "#",
+        })
+      )
+    )
+    .reduce((acc, val) => acc.concat(val), [])
+    .forEach((cube) => cubes.set(getKey(cube), cube));
+
+  const cycle = (cubes: Map<string, Cube>, amount: number): number => {
+    const inner = (
+      cubes: Map<string, Cube>,
+      delta: number
+    ): Map<string, Cube> => {
+      if (delta === amount) {
+        return cubes;
+      } else {
+        const newCubes = new Map<string, Cube>();
+        for (const [key, cube] of cubes) {
+          for (let newCube of expanded(cube.x, cube.y, cube.z, cube.w, 1)) {
+            if (newCubes.has(getKey(newCube))) {
+              continue;
+            }
+            const activeNeighbours = countActiveNeighbours(cubes, newCube, 1);
+            const oldCube = cubes.get(getKey(newCube)) || {
+              ...newCube,
+              active: false,
+            };
+            if (
+              oldCube.active &&
+              (activeNeighbours === 2 || activeNeighbours === 3)
+            ) {
+              newCubes.set(getKey(newCube), {
+                ...newCube,
+                active: true,
+              });
+            } else if (!oldCube.active && activeNeighbours === 3) {
+              newCubes.set(getKey(newCube), {
+                ...newCube,
+                active: true,
+              });
+            }
+          }
+        }
+        return inner(newCubes, delta + 1);
+      }
+    };
+
+    return Array.from(inner(cubes, 0))
+      .map(([_, cube]) => cube)
+      .reduce((prev, cube) => prev + (cube.active ? 1 : 0), 0);
+  };
+  return cycle(cubes, 6);
+}
+
+console.log(`Solution part A: ${runPartA()}`);
+console.log(`Solution part B: ${runPartB()}`);
